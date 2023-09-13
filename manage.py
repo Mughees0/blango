39a7cafb203755a8ed7a40b35c04df264e558256
Abdul Mughees <abdul.mughees@metropolia.fi>
2023-09-13 16:19:14 +0000

    Finish django configurations

--- manage.py
+++ manage.py
@@ -7,6 +7,9 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'blango.settings')
+    os.environ.setdefault("DJANGO_CONFIGURATION", "Dev")
+
+    from configurations.management import execute_from_command_line
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
