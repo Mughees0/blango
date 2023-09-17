087bf44297aaf19543533104695ffb759f7587f7
Abdul Mughees <abdul.mughees@metropolia.fi>
2023-09-17 08:10:07 +0000

    Finish django Allauth

--- blango/settings.py
+++ blango/settings.py
@@ -50,12 +50,17 @@ class Dev(Configuration):
       'django.contrib.contenttypes',
       'django.contrib.sessions',
       'django.contrib.messages',
+      "django.contrib.sites",
       'django.contrib.staticfiles',
       "blango_auth",
       'blog',
       'crispy_forms',
       'crispy_bootstrap5',
       "debug_toolbar",
+      "allauth",
+      "allauth.account",
+      "allauth.socialaccount",
+      "allauth.socialaccount.providers.google",
   ]
 
   MIDDLEWARE = [
@@ -144,6 +149,11 @@ class Dev(Configuration):
   AUTH_USER_MODEL = "blango_auth.User"
   EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
   ACCOUNT_ACTIVATION_DAYS = 7
+  SITE_ID = 1
+  ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+  ACCOUNT_EMAIL_REQUIRED = True
+  ACCOUNT_USERNAME_REQUIRED = False
+  ACCOUNT_AUTHENTICATION_METHOD = "email"
 
   # Default primary key field type
   # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
