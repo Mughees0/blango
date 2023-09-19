82a6ab9544bf14d8aa5ed592fca85bd01c86165b
Abdul Mughees <abdul.mughees@metropolia.fi>
2023-09-19 18:16:14 +0000

    Finish browsable API

--- blango/settings.py
+++ blango/settings.py
@@ -63,6 +63,7 @@ class Dev(Configuration):
       "allauth.socialaccount.providers.google",
       "rest_framework",
       "rest_framework.authtoken",
+      "drf_yasg",
   ]
 
   MIDDLEWARE = [
@@ -165,6 +166,12 @@ class Dev(Configuration):
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticatedOrReadOnly"
     ],
+  }
+  SWAGGER_SETTINGS = {
+    "SECURITY_DEFINITIONS": {
+        "Token": {"type": "apiKey", "name": "Authorization", "in": "header"},
+        "Basic": {"type": "basic"},
+    }
   }
 
   # Default primary key field type
